da1f166b490774ee2471d1d352934a7c661d5f1d
artcevvv <artcevvv@yandex.kz>
2023-12-02 10:57:44 +0600

    ss

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 import memories from "./Assets/memories.png";
+import { useDispatch } from 'react-redux';
 
+import {getPosts} from './actions/posts'
 import { Form, Posts } from "./components/exports";
 import useStyles from './styles'
 
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(()=> {dispatch(getPosts())}, [dispatch])
 
   return (
-    <Container maxWidth="lg">
+      <Container maxWidth="lg">
       <AppBar className= {classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center" className={classes.heading}>
           Memories
